6918b8719d9fcb93f5784d197c3d232cf8b07c52
Cl3arglass <haltaffer@gmail.com>
2019-01-30 22:41:45 -0800

    Done.

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -2,9 +2,11 @@ class GithubRepo
 
   attr_reader :name, :url
 
-  def initialize(hash)
-    @name = hash["name"]
-    @url = hash["html_url"]
+  def initialize(hash = nil)
+  	if hash
+	    @name = hash["name"]
+	    @url = hash["html_url"]
+	end
   end
 
 end
